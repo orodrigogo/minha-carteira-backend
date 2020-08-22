d8b6c84fb366af56e274479bd9afd9624d801f40
rodrigo <rodrigo.rgtic@gmail.com>
2020-08-21 22:38:47 -0300

    feat: express instaled

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Unifafibe');
+const express = require('express');
+const server = express();
+
+server.get('/boasvindas', function(request, response){
+
+  response.send('<h1>API ON!</h1>');
+
+});
+
+
+
+
+
+server.listen(3333, function(){
+  console.log('O PAI TÁ ON!');
+});
