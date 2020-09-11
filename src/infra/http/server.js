7c3fa8e5d7ed9b60fa4f90f3764797f01e2271ae
rodrigo <rodrigo.rgtic@gmail.com>
2020-09-11 20:55:45 -0300

    chore: eslint and prettier

--- src/infra/http/server.js
+++ src/infra/http/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const server = express();
 
+const server = express();
 
 const routes = require('./routes');
 
 server.use(express.json());
 server.use(routes);
 
-
-server.listen(3333, function(){
+server.listen(3333, () => {
+  /* eslint-disable-next-line */
   console.log('Server is running on 3333 port!');
 });
