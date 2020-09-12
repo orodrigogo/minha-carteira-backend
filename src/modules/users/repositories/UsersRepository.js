d9ac8585e925d08c91c89ec5854a11814b0e407e
rodrigo <rodrigo.rgtic@gmail.com>
2020-09-11 22:03:03 -0300

    style: code style

--- src/modules/users/repositories/UsersRepository.js
+++ src/modules/users/repositories/UsersRepository.js
@@ -1,31 +1,29 @@
 const mongo = require('../../../infra/database/mongoose');
 
 class UsersRepository {
-  async add(data){
-
+  async add(data) {
     const user = await mongo
-    .collection('users')
-    .insertOne(data)
-    .then(result => {
-      return result;
-    });  
-    
+      .collection('users')
+      .insertOne(data)
+      .then(result => {
+        return result;
+      });
+
     delete user.ops[0].password;
 
     return user;
-  }  
+  }
 
-  async findByEmail(email){
+  async findByEmail(email) {
     const user = await mongo
-    .collection('users')
-    .findOne({ email })
-    .then(result => {
-      return result;
-    });
-    
+      .collection('users')
+      .findOne({ email })
+      .then(result => {
+        return result;
+      });
+
     return user;
   }
 }
 
 module.exports = new UsersRepository();
-
--- src/modules/users/service/SignUpService.js
+++ src/modules/users/service/SignUpService.js
@@ -1,18 +1,16 @@
 const UsersRepository = require('../repositories/UsersRepository');
 
 class SignUpService {
-  async execute(data){
+  async execute(data) {
     const { name, email, password } = data;
 
     const emailAlreadyUsed = await UsersRepository.findByEmail(email);
 
-    if(emailAlreadyUsed)
+    if (emailAlreadyUsed)
       return { error: 'Email not avaiable. Choise another!' };
-      
-    
 
-    const user = await UsersRepository.add({ name, email, password});
-   
+    const user = await UsersRepository.add({ name, email, password });
+
     return user;
   }
 }
