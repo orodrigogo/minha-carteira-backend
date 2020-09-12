a9dd4fd2a198da32a80592dcde133e210487cc1a
rodrigo <rodrigo.rgtic@gmail.com>
2020-09-11 21:55:15 -0300

    style: code style

--- src/modules/users/controllers/UserController.js
+++ src/modules/users/controllers/UserController.js
@@ -1,33 +1,29 @@
 const SignUpService = require('../service/SignUpService');
 
-class UserController{
-  async create(request, response){
-    const { name, email, password, password_confirm } = request.body;
+class UserController {
+  async create(request, response) {
+    const { name, email, password, passwordConfirm } = request.body;
 
-    if(!name)
-      return response.json({ message: 'name is required'});
-      
-    if(!email)
-      return response.json({ message: 'email is required'});
+    if (!name) return response.json({ message: 'name is required' });
 
-    if(!password)
-      return response.json({ message: 'password is required'});
+    if (!email) return response.json({ message: 'email is required' });
 
-    if(!password_confirm)
-      return response.json({ message: 'password_confirm is required'});
+    if (!password) return response.json({ message: 'password is required' });
 
-    if(password !== password_confirm)
-      return response.json({ message: 'password not match'});
+    if (!passwordConfirm)
+      return response.json({ message: 'password_confirm is required' });
+
+    if (password !== passwordConfirm)
+      return response.json({ message: 'password not match' });
 
     const user = await SignUpService.execute({
-      name, 
-      email, 
-      password
+      name,
+      email,
+      password,
     });
-    
 
     return response.json(user);
-  }  
+  }
 }
 
 module.exports = new UserController();
