aba92a8703e4bc0501b8f4b89bb8de507e93c8bb
rodrigo <rodrigo.rgtic@gmail.com>
2020-11-13 21:21:38 -0300

    chore: config deploy

--- src/infra/http/server.js
+++ src/infra/http/server.js
@@ -9,7 +9,7 @@ const routes = require('./routes');
 server.use(express.json());
 server.use(routes);
 
-server.listen(3333, () => {
+server.listen(process.env.PORT || 8080, () => {
   /* eslint-disable-next-line */
   console.log('Server is running on 3333 port!');
 });
